06024d7aa09ecc5966c9edefd9b159772eef53cb
Maciej Posłuszny <xmachiei@gmail.com>
2016-09-11 12:32:34 +0200

    finished exercise 281 - easy - bases

---
+++ something_about_bases-281/main.js
@@ -0,0 +1,46 @@
+// jshint node: true, esversion: 6
+'use strict';
+
+
+let initHexValues = () => {
+  let result = { };
+  for (let i = 0; i < 6; i++) {
+    let key = String.fromCharCode(97 + i);
+    let value = 11 + i;
+    result[key] = value;
+  }
+  return result;
+};
+
+const HEX = initHexValues();
+
+let isValidInput = (input) => {
+  if (!input || input === "") {
+    return false;
+  }
+  const REGEX = /[0-9]*[a-f]*/g;
+  return REGEX.test(input);
+};
+
+let isHexChar = (char) => {
+  return /[a-f]/.test(char);
+};
+
+let getMinimalBase = (input) => {
+  if (!isValidInput(input)) {
+    return "invalid input";
+  }
+  let base;
+  let temp = input.split('').sort();
+  temp = temp[temp.length-1];
+  if (isHexChar(temp)) {
+    base = HEX[temp];
+  }
+  else {
+    base = parseInt(temp, 10) + 1;
+  }
+  return `base ${base} => it's ${parseInt(input, base)} in base 10`;
+};
+
+// usage: node main.js 101 -> 2
+console.log(getMinimalBase(process.argv[2]));
